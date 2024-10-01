0a4afa1d12ca026d8725db31731656fe7fb5e26d
BaraaZughbi <bzughbi7@gmail.com>
2024-10-01 07:47:35 +0200

    Update maina.lua

--- maina.lua
+++ maina.lua
@@ -3,3 +3,5 @@ local ENV = "DEV"
 if ENV == "DEV" then
     loadstring(game:HttpGet(""))
 end]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BaraaZughbi/BaraaZughbi/refs/heads/main/maina.lua"))()
